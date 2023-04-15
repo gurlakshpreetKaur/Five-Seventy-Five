592ef07cd2094d7da4323f39ed80085e5e750f32
gurlakshpreetKaur <123385856+gurlakshpreetKaur@users.noreply.github.com>
2023-04-15 19:49:13 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,13 +3,7 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "@firebase/auth";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBG-EH-IAVXCn4a15jvnfErZYp6yIiHTMs",
-    authDomain: "five-seventy-five-827cb.firebaseapp.com",
-    projectId: "five-seventy-five-827cb",
-    storageBucket: "five-seventy-five-827cb.appspot.com",
-    messagingSenderId: "791078910050",
-    appId: "1:791078910050:web:91f76e6c4c214bf387393b",
-    measurementId: "G-ZDYC274MKQ"
+    //firebase config data
 };
 
 const app = initializeApp(firebaseConfig);
