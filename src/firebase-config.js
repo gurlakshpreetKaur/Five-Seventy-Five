0e3acd922859bde7ef43efa3e81da35aa49101cc
gurlakshpreetKaur <123385856+gurlakshpreetKaur@users.noreply.github.com>
2023-04-15 19:37:52 +0530

    Add Files

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -3,7 +3,13 @@ import { getFirestore } from "@firebase/firestore";
 import { getAuth } from "@firebase/auth";
 
 const firebaseConfig = {
-    //firebase api data
+    apiKey: "AIzaSyBG-EH-IAVXCn4a15jvnfErZYp6yIiHTMs",
+    authDomain: "five-seventy-five-827cb.firebaseapp.com",
+    projectId: "five-seventy-five-827cb",
+    storageBucket: "five-seventy-five-827cb.appspot.com",
+    messagingSenderId: "791078910050",
+    appId: "1:791078910050:web:91f76e6c4c214bf387393b",
+    measurementId: "G-ZDYC274MKQ"
 };
 
 const app = initializeApp(firebaseConfig);
